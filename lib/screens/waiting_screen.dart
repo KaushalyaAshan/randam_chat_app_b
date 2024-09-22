a2629c3fbb42dad52fc9213d350aafb7677466c3
Y.P.Ashan Kaushalya Premarathna <akr10010production@gmail.com>
2024-09-22 18:21:18 +0530

    Update waiting_screen.dart

--- lib/screens/waiting_screen.dart
+++ lib/screens/waiting_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lottie/lottie.dart';
-
 import '../services/auth_service.dart';
 import 'chat_screen.dart';
 
